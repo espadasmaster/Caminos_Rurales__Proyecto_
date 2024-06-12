9e55007545f10267548095e2e9c5bd8446c82161
Juan Benavidez <gohanelssj1@gmail.com>
2024-06-12 11:36:26 -0300

    Add CSS for centering and styling p5.js canvas

--- Caminos_Rurales__Proyecto_.js
+++ Caminos_Rurales__Proyecto_.js
@@ -1,33 +1,75 @@
 
-let ini;
 let botonInicio;
-let x = 1360;
-let y = 600;
+let portada;
+
+var fade;
+var fadeAmount = 1;
+var frames = [];
+var whichFrame = 0;
+var delay = 1000;  //milisegundos para el delay
+var nextTimer = 0;
 
 
-function preload() {
-  // Carga la imagen antes de que comience el programa
-  img = loadImage('imag/inicio.png');
+function preload(){
+ portada = loadImage("inicio.png");
+ //inicio
+ frames[0] = loadImage("Inicio/escena1.png");
+ frames[1] = loadImage("Inicio/escena2.png");
+ frames[2] = loadImage("Inicio/escena3.png");
+ frames[3] = loadImage("Inicio/escena4.png");
+  //cueva
 }
 
+function transicion(){
+    if (fade>255) {
+      //fade += fadeAmount; 
+      fadeAmount=-7;
+
+    }
+    else if (fade < 0){
+      fadeAmount = 0;
+      fade = 0;
+    }
+}
 
 function setup() {
 createCanvas(1360, 600);
+nextTimer = millis() + delay;
+ //boton de inicio
+
+botonInicio = createButton("comenzar");
+botonInicio.position(width/2 - 50, height/2);
+botonInicio.mouseClicked(draw);
 
 
-ini = loadImage('imag/inicio.png');
-
- botonInicio = createButton('iniciar');
-  botonInicio.position(650, 300);
-  botonInicio.mousePressed(draw);
-  
 }
 
 
+
 function draw() {
-background(100);
+background(0);
+ fade += fadeAmount;
+ transicion();
+ 
+ image(portada, 210 , 0, 900, 600);
+ 
+ 
+  image(frames[whichFrame], 0, 0, 1360, 600);
 
-image(ini, x, y);
+if (millis() > nextTimer) {
+    whichFrame = whichFrame + 1;
+    // vuelve al inicio en un loop
+    if (whichFrame >= frames.length) {
+      whichFrame = 0;
+    }
+    // resetea el tiempo para el siguiente frame 
+    nextTimer = millis() + delay;
+  }
+ 
 
+}
 
+function mouseClicked(){
+  
+  
 }
